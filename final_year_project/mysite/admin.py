a9048dd8eef5e6d9ec245a4c7196e5dd0b6bbd64
GMC851 <GMC851@student.bham.ac.uk>
2022-03-15 22:28:26 +0000

    changes

--- final_year_project/mysite/admin.py
+++ final_year_project/mysite/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post, Article, Event, Donation, BarberTile
+from .models import Post, Article, Event, Donation, BarberTile, LikedTag, Comment
 
 admin.site.register(Post)
 admin.site.register(Article)
 admin.site.register(Event)
 admin.site.register(Donation)
 admin.site.register(BarberTile)
+admin.site.register(LikedTag)
+admin.site.register(Comment)
 
 #here I register what models I want the admin users to access
